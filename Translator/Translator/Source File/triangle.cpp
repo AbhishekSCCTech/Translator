bb7fefc6d152a971bc6cec82d2c6a5d37c321f6a
AbhishekSCCTech <abhishek.suryawanshi@cctech.co.in>
2024-10-11 12:09:20 +0530

    replied

--- Translator/Translator/Source File/triangle.cpp
+++ Translator/Translator/Source File/triangle.cpp
@@ -7,10 +7,13 @@ Triangle::Triangle(Point p1, Point p2, Point p3) : p1(p1), p2(p2), p3(p3) {}
 Triangle::~Triangle() {}
 
 void Triangle::get_coord() const {
+    
     p1.get_coord();
     p2.get_coord();
     p3.get_coord();
 }
+
+
 
 
 
